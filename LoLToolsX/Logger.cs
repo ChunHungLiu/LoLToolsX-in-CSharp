54fd86498fc3ca1b52f0271ad10d47cc1f2be146
NitroXenon <peter9104@hotmail.com>
2014-06-25 16:48:30 +0800

    Added Logger

---
+++ LoLToolsX/Logger.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace LoLToolsX
+{
+    public static class Logger
+    {
+
+        /// <summary>
+        /// Logger
+        /// </summary>
+
+        public enum LogType
+        {
+            Error, Info, Crash, Exception, Game, Fml,
+        }
+
+        public static string file;
+
+
+        public static void start()
+        {
+            if (!Directory.Exists(Directory.GetCurrentDirectory() + @"\Logs"))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\Logs");
+                DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory() + @"\Logs");
+                di.Refresh();
+            }
+                GC.Collect();
+                file = System.IO.Path.Combine(Directory.GetCurrentDirectory() + @"\Logs\" + "Log.txt");
+                if (!File.Exists(Directory.GetCurrentDirectory() + @"\Logs\" + "Log.txt"))
+                    File.Create(Directory.GetCurrentDirectory() + @"\Logs\" + "Log.txt");
+                FileStream fs = new FileStream(Directory.GetCurrentDirectory() + @"\Logs\" + "Log.txt", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                if (Debug.debug)
+                {
+                    wpfLog wpLog = new wpfLog();
+                    wpLog.Show();
+                }
+            }
+        
+
+        public static void stop()
+        {
+            if (Debug.debug)
+            {
+                wpfLog wpLog = new wpfLog();
+                wpLog.Show();
+            }
+        }
+        private static string writeInfo(LogType type = LogType.Info)
+        {
+            switch (type)
+            {
+                case LogType.Error:
+                    return (DateTime.Now.ToString(CultureInfo.InvariantCulture) + "錯誤: ");
+                case LogType.Info:
+                    return (DateTime.Now.ToString(CultureInfo.InvariantCulture) + "信息: ");
+                case LogType.Crash:
+                    return (DateTime.Now.ToString(CultureInfo.InvariantCulture) + "崩潰: ");
+                case LogType.Exception:
+                    return (DateTime.Now.ToString(CultureInfo.InvariantCulture) + "異常: ");
+                case LogType.Game:
+                    return (DateTime.Now.ToString(CultureInfo.InvariantCulture) + "遊戲: ");
+                case LogType.Fml:
+                    return (DateTime.Now.ToString(CultureInfo.InvariantCulture) + "FML : ");
+                default:
+                    return (DateTime.Now.ToString(CultureInfo.InvariantCulture) + "信息: ");
+            }
+        }
+
+        //寫入記錄檔
+        private static void write(string str, LogType type = LogType.Info)
+        {
+            GC.Collect();   //釋放資源 以免不能存取
+            FileStream fs = new FileStream(file, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            sw.WriteLine(writeInfo(type) + str);
+            sw.Close();
+            fs.Close();
+            if (Debug.debug)
+            {
+                wpfLog wpLog = new wpfLog();
+                wpLog.WriteLine(str, type);
+            }
+        }
+
+        private static void write(Stream s, LogType type = LogType.Info)
+        {
+            StreamReader sr = new StreamReader(s);
+            write(sr.ReadToEnd(), type);
+            sr.Close();
+            if (Debug.debug)
+            {
+                wpfLog wpLog = new wpfLog();
+                s.Position = 0;
+                wpLog.WriteLine(sr.ReadToEnd(), type);
+            }
+        }
+
+        static public void log(string str, LogType type = LogType.Info)
+        {
+            write(str, type);
+        }
+
+        static public void log(Stream s, LogType type = LogType.Info)
+        {
+            StreamReader sr = new StreamReader(s);
+            sr.Close();
+            write(sr.ReadToEnd(), type);
+            
+        }
+        
+        // Log Exception
+        static public void log(Exception ex, LogType type = LogType.Exception)
+        {
+            StringBuilder message = new StringBuilder();
+            message.AppendLine(ex.Source);
+            message.AppendLine(ex.ToString());
+            message.AppendLine(ex.Message);
+            foreach (DictionaryEntry data in ex.Data)
+            message.AppendLine(string.Format("Key:{0}\nValue:{1}", data.Key, data.Value));
+            message.AppendLine(ex.StackTrace);
+            var iex = ex;
+            while (iex.InnerException != null)
+            {
+                message.AppendLine("------------------------");
+                iex = iex.InnerException;
+                message.AppendLine(iex.Source);
+                message.AppendLine(iex.ToString());
+                message.AppendLine(iex.Message);
+                foreach (DictionaryEntry data in ex.Data)
+                message.AppendLine(string.Format("Key:{0}\nValue:{1}", data.Key, data.Value));
+                message.AppendLine(iex.StackTrace);
+            }
+            write(message.ToString(), type);
+        }
+
+        static public void info(string message)
+        {
+            Logger.log(message);
+        }
+
+        static public void error(string message)
+        {
+            Logger.log(message, LogType.Error);
+        }
+
+        static public void error(Exception ex)
+        {
+            Logger.log(ex);
+        }
+    }
+}
