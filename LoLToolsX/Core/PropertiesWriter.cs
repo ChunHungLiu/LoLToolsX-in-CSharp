0be45ba39caf56a466bfdfe2e519c96515d0ce80
NitroXenon <peter9104@hotmail.com>
2014-12-10 22:55:33 +0800

    Added new method to edit lol.properties.

---
+++ LoLToolsX/Core/PropertiesWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LoLToolsX.Core
+{
+    class PropertiesWriter
+    {
+        Server server;
+
+        public PropertiesWriter(Server _server)
+        {
+            this.server = _server;
+        }
+
+        public void Write()
+        {
+            using (StreamWriter writer = new StreamWriter(Variable.PropertiesFile,false,Encoding.UTF8))
+            {
+                foreach (string str in Utility.GetPropertiesFile(this.server))
+                {
+                    writer.WriteLine(str);
+                }
+            }
+        }
+    }
+}
